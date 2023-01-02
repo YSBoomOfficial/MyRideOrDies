9c7c442945fc1f613754e558ea9ab8c6b4b247a2
Yash <63893378+YSBoomOfficial@users.noreply.github.com>
2023-01-02 22:15:40 +0000

    Initial Commit

---
+++ MyRideOrDies/MyRideOrDiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyRideOrDiesApp.swift
+//  MyRideOrDies
+//
+//  Created by Yash Shah on 02/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyRideOrDiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
